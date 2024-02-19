5a5e282af9088263ee1facc6efde57303f4094b9
antonpool <kaspera@yandex.ru>
2024-02-19 13:32:56 +0300

    создал таблицы

--- db/admin.py
+++ db/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Table)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [ "name", 'date']
--- db/models.py
+++ db/models.py
@@ -1,5 +1,22 @@
-# from django.db import models
+from django.db import models
 
-# # Create your models here.
-# class Table (model.Models):
-#     title = model.CharField(max_length)
+# Create your models here.
+class Table (models.Model):
+    name = models.CharField( 'название темы', max_length=200)
+    title = models.CharField('основная мысль', max_length=200)
+    text_all = models.TextField('содержимое статьи')
+    text_code = models.TextField('как выглядт код',blank=True, null=True)
+    image= models.ImageField('фото', upload_to="images/", blank=True, null=True)
+    date = models.DateTimeField('дата создания', auto_now=True)
+
+    slug = models.SlugField('ссылка для статьи', max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = ("Таблица")
+        verbose_name_plural = ("Таблицы")
+
+    # def get_absolute_url(self):
+    # return reverse("_detail", kwargs={"pk": self.pk})
